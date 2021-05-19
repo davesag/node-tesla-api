225c31d243f5b5cef226af2af059d4eb3a32d76a
Dave Sag <davesag@gmail.com>
2021-05-19 13:17:11 +1000

    n/a upgraded dependencies and re-prettiered

--- src/utils/genericAuthenticatedGet.js
+++ src/utils/genericAuthenticatedGet.js
@@ -1,8 +1,10 @@
 const { getTransport } = require('./transport')
 
-const genericAuthenticatedGet = path => async ({ token, id }) => {
-  const { get } = getTransport({ token })
-  return get(`/api/1/vehicles/${id}/${path}`)
-}
+const genericAuthenticatedGet =
+  path =>
+  async ({ token, id }) => {
+    const { get } = getTransport({ token })
+    return get(`/api/1/vehicles/${id}/${path}`)
+  }
 
 module.exports = genericAuthenticatedGet
